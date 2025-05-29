89b8726b8c2d5b3ad4923d2dea57e0b34e6594bc
Habib <adebayohabib7@gmail.com>
2025-05-29 15:49:36 +0100

    Update Header.tsx

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -9,7 +9,7 @@ const Header: React.FC = () => {
   const { isAuthenticated } = useAuthStore();
   return (
     <header className="bg-white shadow">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="w-full max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex">
             <Link to="/" className="flex items-center">
